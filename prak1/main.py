5c2b0876e08c3e229e6c7aa27bd1d3271e89858b
mrmin50000 <danilast2007@gmail.com>
2025-09-06 13:17:51 +0300

    forpc

--- prak1/main.py
+++ prak1/main.py
@@ -2,12 +2,22 @@ import socket
 import os
 from pathlib import Path
 from commands import *
+import sys
 
 HOSTNAME = socket.gethostname()
 USERNAME = os.getlogin()
 CWD = os.getcwd()
 HOME = Path.home()
+VFS_PATH = sys.argv[1]
+SHITRC_PATH = sys.argv[2]
 
+if not os.path.isfile(VFS_PATH):
+    print("not found vfs file")
+    exit()
+if not os.path.isfile(SHITRC_PATH):
+    print("not found shitrc file")
+    exit()
+print(f'vfs file: {VFS_PATH}, shitrc file: {SHITRC_PATH}')
 
 while True:
     CWD = os.getcwd()
